119c0c4d82247003317797bfafab171f6ade927a
Ondrej Ptak <optak@redhat.com>
2014-02-17 18:37:18 +0100

    bz1061451-scl-doc: LICENSE, man page and README check

---
+++ Regression/bz1061451-scl-doc/runtest.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/nginx/Regression/bz1061451-scl-doc
+#   Description: check if LICENCE, README and man page are in collection runtime package
+#   Author: Ondrej Ptak <optak@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2014 Red Hat, Inc.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include Beaker environment
+. /usr/bin/rhts-environment.sh || exit 1
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+PACKAGES=${PACKAGES:-"nginx"}
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlAssertRpm --all
+        rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
+        rlRun "pushd $TmpDir"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun "scl_package=$(scl -l|grep nginx|head -1)" 0 "getting version of package in rhscl"
+        rlRun "runtime_package=${scl_package}-runtime"
+        rlRun "rpm -ql $runtime_package|grep '/opt/rh/$scl_package/root/usr/share/doc/${runtime_package}.*LICENSE'"
+        rlRun "rpm -ql $runtime_package|grep '/opt/rh/$scl_package/root/usr/share/doc/${runtime_package}.*README'"
+        rlRun "man $scl_package > man_content" 0 "getting man page content"
+        rlAssertGrep "Software Collection" man_content
+        rlAssertGrep "scl enable $scl_package" man_content
+        rlAssertNotGrep "%%.*%%" man_content # unexpanded variables
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+        rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
+    rlPhaseEnd
+rlJournalPrintText
+rlJournalEnd
