64faf69258dccb114154eda2d6d8320f1f2010d3
Ondrej Ptak <optak@redhat.com>
2014-04-20 12:47:32 +0200

    bz1052935-selinux-context-of-nginx14-nginx: initscripts'context only on rhel6

--- Regression/bz1052935-selinux-context-of-nginx14-nginx/runtest.sh
+++ Regression/bz1052935-selinux-context-of-nginx14-nginx/runtest.sh
@@ -45,7 +45,9 @@ rlJournalStart
         rlRun "ls -d --scontext \$(rpm -ql $NGINX_RPM) > files_context.log"\
             0 "getting selinux context of rpm's files"
         rlAssertNotGrep ":initrc_exec_t" files_context.log
-        rlAssertGrep ":httpd_initrc_exec_t.*/etc/init.d/nginx" files_context.log
+        if rlIsRHEL 6; then
+            rlAssertGrep ":httpd_initrc_exec_t.*/etc/init.d/nginx" files_context.log
+        fi
         rlAssertGrep ":httpd_exec_t.*/usr/sbin/nginx" files_context.log
         rlAssertGrep ":httpd_config_t.*/etc/nginx" files_context.log
         rlAssertGrep ":httpd_log_t.*/var/log/nginx14" files_context.log
