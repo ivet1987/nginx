b972d2484de2dd9d251b9dbc47a935f7bf88e65c
Ondrej Ptak <optak@localhost.localdomain>
2014-07-03 09:44:50 +0200

    bz1061451-scl-doc: test parametrized

--- Regression/bz1061451-scl-doc/runtest.sh
+++ Regression/bz1061451-scl-doc/runtest.sh
@@ -35,18 +35,18 @@ PACKAGES=${PACKAGES:-"nginx"}
 rlJournalStart
     rlPhaseStartSetup
         rlAssertRpm --all
+        rlRun "rlImport nginx/nginx"
         rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
         rlRun "pushd $TmpDir"
     rlPhaseEnd
 
     rlPhaseStartTest
-        rlRun "scl_package=$(scl -l|grep nginx|head -1)" 0 "getting version of package in rhscl"
-        rlRun "runtime_package=${scl_package}-runtime"
-        rlRun "rpm -ql $runtime_package|grep '/opt/rh/$scl_package/root/usr/share/doc/${runtime_package}.*LICENSE'"
-        rlRun "rpm -ql $runtime_package|grep '/opt/rh/$scl_package/root/usr/share/doc/${runtime_package}.*README'"
-        rlRun "man $scl_package > man_content" 0 "getting man page content"
+        rlRun "runtime_package=${nginxCOLLECTION_NAME}-runtime"
+        rlRun "rpm -ql $runtime_package|grep '/opt/rh/$nginxCOLLECTION_NAME/root/usr/share/doc/${runtime_package}.*LICENSE'"
+        rlRun "rpm -ql $runtime_package|grep '/opt/rh/$nginxCOLLECTION_NAME/root/usr/share/doc/${runtime_package}.*README'"
+        rlRun "man $nginxCOLLECTION_NAME > man_content" 0 "getting man page content"
         rlAssertGrep "Software Collection" man_content
-        rlAssertGrep "scl enable $scl_package" man_content
+        rlAssertGrep "scl enable $nginxCOLLECTION_NAME" man_content
         rlAssertNotGrep "%%.*%%" man_content # unexpanded variables
 
     rlPhaseStartCleanup
