31fac5ed3661244763833d468e1a83a683ceb615
Ondrej Ptak <optak@localhost.localdomain>
2014-07-22 12:11:21 +0200

    bz1052935-selinux-context-of-nginx14-nginx: fixed expected selinux context

--- Regression/bz1052935-selinux-context-of-nginx14-nginx/runtest.sh
+++ Regression/bz1052935-selinux-context-of-nginx14-nginx/runtest.sh
@@ -45,7 +45,7 @@ rlJournalStart
             0 "getting selinux context of rpm's files"
         rlAssertNotGrep ":initrc_exec_t" files_context.log
         if rlIsRHEL 6; then
-            rlAssertGrep ":httpd_initrc_exec_t.*/etc/init.d/nginx" files_context.log
+            rlAssertGrep ":httpd_initrc_exec_t.*init.d/nginx" files_context.log
         fi
         rlAssertGrep ":httpd_exec_t.*/usr/sbin/nginx" files_context.log
         rlAssertGrep ":httpd_config_t.*/etc/nginx" files_context.log
