03b03f9d8604246e5e83a4881fccab8bb4cc5af3
Martin Frodl <mfrodl@redhat.com>
2014-07-30 15:35:04 +0200

    nginx-upstream-testsuite: SCGI & uWSGI testing included

--- Regression/nginx-upstream-testsuite/runtest.sh
+++ Regression/nginx-upstream-testsuite/runtest.sh
@@ -18,12 +18,22 @@
 . /usr/bin/rhts-environment.sh || exit 1
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
-TARVERSION=fb366c51eac6
 LOOKASIDE=${LOOKASIDE:-http://download.eng.bos.redhat.com/qa/rhts/lookaside/}
 
+TARVERSION=fb366c51eac6
 TARSTUB=nginx-tests-${TARVERSION}
 TARBALL=${TARSTUB}.tar.gz
 TARURL=${LOOKASIDE}/${TARBALL}
+
+SCGIVERSION=0.6
+SCGISTUB=SCGI-${SCGIVERSION}
+SCGITARBALL=${SCGISTUB}.tar.gz
+SCGIURL=${LOOKASIDE}/${SCGITARBALL}
+
+UWSGIVERSION=2.0.6
+UWSGISTUB=uwsgi-${UWSGIVERSION}
+UWSGITARBALL=${UWSGISTUB}.tar.gz
+UWSGIURL=${LOOKASIDE}/${UWSGITARBALL}
 
 WHITELIST=$PWD/whitelist.txt
 
@@ -35,6 +45,44 @@ rlJournalStart
         rlAssertBinaryOrigin nginx
         rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
         rlRun "pushd $TmpDir"
+
+        for MODULE in Module::Build FCGI SCGI; do
+            perl -e "use $MOD" ||
+            rlRun "yes | perl -MCPAN -e 'install $MOD'" 0 "Installing $MOD"
+        done
+
+        # Manual installation in case the automated way above should fail
+        #
+        # install SCGI to test if it works with nginx
+        #perl -e 'use Module::Build;' || 
+        #rlRun "yes | perl -MCPAN -e 'install Module::Build'" 0 \
+        #    "Installing Module::Build"
+
+        #perl -e 'use SCGI;' || {
+        #    rlRun "wget $SCGIURL" 0 "Downloading SCGI package"
+        #    rlRun "tar -xvf $SCGITARBALL" 0 "Extracting archive"
+        #    rlRun "pushd $SCGISTUB"
+        #    rlRun "perl Build.PL"
+        #    rlRun "./Build"
+        #    rlRun "./Build test"
+        #    rlRun "./Build install"
+        #    rlRun "popd"
+        #}
+
+        # same for uWSGI
+        rlRun "wget $UWSGIURL" 0 "Downloading uWSGI package"
+        rlRun "tar -xvf $UWSGITARBALL" 0 "Extracting archive"
+        rlRun "pushd uwsgi-2.0.6"
+        # I am not sure if this is necessary; feel free to remove this
+        # commented section if the test does not make any trouble; uncomment
+        # otherwise:
+        #rlRun "yum groupinstall \"Development Tools\"" 0 \
+        #    "Installing Development Tools"
+        rlRun "make"
+        rlRun "export PATH=$PATH:$(pwd)"
+        rlRun "popd"
+
+        # download upstream test suite
         rlRun "wget $TARURL"
         rlRun "tar -xzvf ${TARBALL}"
         rlRun "pushd ${TARSTUB}"
@@ -45,7 +93,14 @@ rlJournalStart
         #rlRun "TEST_NGINX_BINARY=\$(which nginx) prove ." 0 "Run whole test suite"
     
         # Run whitelisted tests is known to pass with 1.4.x
-        rlRun "TEST_NGINX_BINARY=\$(which nginx) xargs prove < ${WHITELIST}" 0 "Run test suite w/whitelist"
+        rlRun "TEST_NGINX_BINARY=\$(which nginx) xargs prove < ${WHITELIST} | tee test.log" 0 "Run test suite w/whitelist"
+
+        # Make sure that all the tests actually passed and were not merely
+        # skipped
+        sed -i -n '/\.\/.*\.t/p' test.log
+        while read line; do
+            rlRun "echo $line | grep 'ok\s*$'" 0 "$line"
+        done < test.log
     rlPhaseEnd
 
     rlPhaseStartCleanup
