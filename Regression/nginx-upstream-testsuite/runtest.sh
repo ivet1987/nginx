b1b088a9187a102135ba2bb271386af79ca16e6f
Martin Frodl <mfrodl@redhat.com>
2014-10-22 17:23:10 +0200

    nginx-upstream-testsuite: fixed mysterious SCGI installation fail

--- Regression/nginx-upstream-testsuite/runtest.sh
+++ Regression/nginx-upstream-testsuite/runtest.sh
@@ -46,6 +46,10 @@ rlJournalStart
         rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
         rlRun "pushd $TmpDir"
 
+        # Deactivate Perl module local::lib, which is sometimes activated by
+        # default and makes an unpleasant mess in search paths
+        rlRun "eval $(perl -Mlocal::lib=--deactivate-all)"
+
         # Install Perl modules to test them with nginx
         for MOD in Net::SSLeay FCGI SCGI; do
             perl -e "use $MOD" ||
