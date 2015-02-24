0681e0c3f9e81d90b1094b4c9e582ea2d20fbb9e
David Kutalek <dkutalek@redhat.com>
2015-02-24 18:46:16 +0100

    Sanity/php-fpm-integration: support for rh-php56 collection

--- Sanity/php-fpm-integration/runtest.sh
+++ Sanity/php-fpm-integration/runtest.sh
@@ -30,7 +30,9 @@
 . /usr/bin/rhts-environment.sh || exit 1
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
-if echo $COLLECTIONS | grep php55; then
+if echo $COLLECTIONS | grep php56; then
+    FPM="rh-php56-php-fpm"
+elif echo $COLLECTIONS | grep php55; then
     FPM="php55-php-fpm"
 elif echo $COLLECTIONS | grep php54; then
     FPM="php54-php-fpm"
