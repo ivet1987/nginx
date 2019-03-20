f5fab98bf46545669cac1053a2daa753a76e9ddc
Maryna Nalbandian <mnalband@redhat.com>
2019-03-20 14:18:47 +0100

    Update & generalize nginx upstream testsuite

--- Regression/nginx-upstream-testsuite/runtest.sh
+++ Regression/nginx-upstream-testsuite/runtest.sh
@@ -18,37 +18,18 @@
 . /usr/bin/rhts-environment.sh || exit 1
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
-LOOKASIDE=${LOOKASIDE:-http://download.eng.bos.redhat.com/qa/rhts/lookaside/}
-
-TARVERSION=fb366c51eac6
-TARSTUB=nginx-tests-${TARVERSION}
-TARBALL=${TARSTUB}.tar.gz
-TARURL=${LOOKASIDE}/${TARBALL}
-
-UWSGIVERSION=2.0.6
-UWSGISTUB=uwsgi-${UWSGIVERSION}
-UWSGITARBALL=${UWSGISTUB}.tar.gz
-UWSGIURL=${LOOKASIDE}/${UWSGITARBALL}
-
-SSLVERSION=1.997
-SSLSTUB=IO-Socket-SSL-${SSLVERSION}
-SSLTARBALL=${SSLSTUB}.tar.gz
-SSLURL=${LOOKASIDE}/${SSLTARBALL}
-
 WHITELIST=$PWD/whitelist.txt
-
 PACKAGES=${PACKAGES:-nginx}
 
 rlJournalStart
     rlPhaseStartSetup
         rlAssertRpm --all
-        rlAssertBinaryOrigin nginx
-
+        rlAssertBinaryOrigin $PACKAGES
+        rlRun "rlImport nginx/nginx" || rlDie
         # Deactivate Perl module local::lib, which is sometimes activated by
         # default and makes an unpleasant mess in search paths
         rlRun "eval $(perl -Mlocal::lib=--deactivate-all)" 0-255 \
               "Deactivating local::lib"
-
         if rlIsRHEL 6; then
             rlRun "cp Config.pm /usr/share/perl5/CPAN/Config.pm" 0
                   "Copying CPAN configuration file"
@@ -57,8 +38,8 @@ rlJournalStart
         fi
 
         rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
+        rlRun "cp Nginx.pm $TmpDir"
         rlRun "pushd $TmpDir"
-
         # Install Perl modules to test them with nginx
         for MOD in Net::SSLeay FCGI SCGI; do
             perl -e "use $MOD" || rlRun "cpan $MOD" 0 "Installing $MOD"
@@ -68,35 +49,33 @@ rlJournalStart
         # one currently available in repository, so it has to be installed
         # manually)
         if ! perl -e "use IO::Socket::SSL"; then
-            rlRun "wget $SSLURL" 0 "Downloading IO-Socket-SSL module"
-            rlRun "tar -xvf $SSLTARBALL" 0 "Extracting archive"
-            rlRun "pushd $SSLSTUB"
+            rlRun "git clone https://github.com/noxxi/p5-io-socket-ssl.git"
+            rlRun "cd $TmpDir/p5-io-socket-ssl"
             rlRun "yes | perl Makefile.PL"
             rlRun "make"
             rlRun "make install"
-            rlRun "popd"
         fi
 
         # Same for uWSGI
-        rlRun "wget $UWSGIURL" 0 "Downloading uWSGI package"
-        rlRun "tar -xvf $UWSGITARBALL" 0 "Extracting archive"
-        rlRun "pushd $UWSGISTUB"
+        rlRun "cd $TmpDir"
+        rlRun "git clone https://github.com/unbit/uwsgi.git"
+        rlRun "cd $TmpDir/uwsgi"
         rlRun "make"
         rlRun "export PATH=$PATH:$(pwd)"
-        rlRun "popd"
 
         # Download upstream test suite
-        rlRun "wget $TARURL"
-        rlRun "tar -xzvf ${TARBALL}"
-        rlRun "pushd ${TARSTUB}"
+        rlRun "cd $TmpDir"
+        rlRun "git clone https://github.com/nginx/nginx-tests.git"
+        rlRun "cd $TmpDir/nginx-tests"
+        rlRun "rm -f lib/Test/Nginx.pm && cp $TmpDir/Nginx.pm $TmpDir/nginx-tests/lib/Test"
     rlPhaseEnd
 
     rlPhaseStartTest
         # Uncomment this to run entire test suite:
         #rlRun "TEST_NGINX_BINARY=\$(which nginx) prove ." 0 "Run whole test suite"
-    
         # Run whitelisted tests is known to pass with 1.4.x
-        rlRun "TEST_NGINX_BINARY=\$(which nginx) xargs prove < ${WHITELIST} | tee test.log" 0 "Run test suite w/whitelist"
+        MODULES="${nginxROOTPREFIX}/usr/lib64/nginx/modules"
+        rlRun "TEST_NGINX_BINARY=\$(which nginx) TEST_NGINX_MODULES=$MODULES xargs prove < ${WHITELIST} | tee test.log" 0 "Run test suite w/whitelist"
 
         # For each test, check whether it ran successfully (PASS), was skipped
         # (WARN) or failed (FAIL).
@@ -113,8 +92,6 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartCleanup
-        rlRun "popd"
-        rlRun "popd"
         rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
     rlPhaseEnd
 rlJournalPrintText
