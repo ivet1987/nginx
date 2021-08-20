51fbe7a05d116420e4ad9d14e8eaf628e2e78e36
Iveta Cesalova <icesalov@redhat.com>
2021-08-20 11:05:34 +0200

    new test for bz1991600

---
+++ Regression/bz1991600-Add-logo-symlink-required-by-new-testpage/runtest.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/nginx/Regression/bz1991600-Add-logo-symlink-required-by-new-testpage
+#   Description: Test for BZ#1991600 (Add logo symlink required by new testpage)
+#   Author: Iveta Cesalova <icesalov@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2021 Red Hat, Inc.
+#
+#   This program is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 2 of
+#   the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see http://www.gnu.org/licenses/.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include Beaker environment
+. /usr/bin/rhts-environment.sh || exit 1
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+PACKAGES=${PACKAGES:-"nginx"}
+SPEC="/root/rpmbuild/SPECS/nginx.spec"
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlAssertRpm --all
+        rlRun "rlImport nginx/nginx"
+        rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
+        rlRun "pushd $TmpDir"
+        rlRun "rlFetchSrcForInstalled $PACKAGES"
+        rlRun "rlServiceStart $nginxHTTPD"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun "rpm -ivh ${PACKAGE}*.src.rpm"
+        rlRun "cat $SPEC | grep /nginx/html/system_noindex_logo.png"
+        rlRun "curl http://localhost/ > output"
+        rlAssertNotDiffer /usr/share/nginx/html/index.html output
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "rlServiceStop $nginxHTTPD"
+        rlRun "popd"
+        rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
+    rlPhaseEnd
+rlJournalPrintText
+rlJournalEnd
