2da7a34a92b1b8018fb29d6f5a8c402da161f095
Iveta Cesalova <icesalov@redhat.com>
2023-02-06 23:02:07 +0100

    adjust PACKAGES variable

--- Sanity/nginx-sanity-test/runtest.sh
+++ Sanity/nginx-sanity-test/runtest.sh
@@ -34,7 +34,7 @@ URL=http://127.0.0.1:81/
 RPURL=http://127.0.0.1:81/rp/
 PHPURL=http://127.0.0.1:81/info.php
 
-PACKAGES=${PACKAGES:-"nginx nginx-core"}
+PACKAGES=${PACKAGES:-"nginx"}
 
 rlJournalStart
     rlPhaseStartSetup
